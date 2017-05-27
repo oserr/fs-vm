ada779caf684c1e6b3ef4403e73d881267b62d63
Omar Serrano <oser520@gmail.com>
2017-05-27 15:13:11 -0700

    Use DB to insert and read posts

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,24 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2 as db
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
+DBNAME = 'forum'
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  conn = db.connect(database=DBNAME)
+  c = conn.cursor()
+  c.execute('select content, time, rom posts order by time desc')
+  l = c.fetchall()
+  conn.close()
+  return l
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+  conn = db.connect(database=DBNAME)
+  c = conn.cursor()
+  c.execute('insert into posts values (%s)', (content,))
+  conn.commit()
+  conn.close()
